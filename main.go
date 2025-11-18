11a29b0556dafb0c852e2a794f881e2bc3c7f0ca
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-11-18 13:30:35 +0100

    wip

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type User struct {
 	Firstname string
@@ -9,17 +11,34 @@ type User struct {
 	Age       int
 }
 
-func newUser(firstname, lastname, state string, age int) *User {
+type UserError struct {
+	Msg string
+}
+
+func (e *UserError) Error() string {
+	return e.Msg
+}
+
+func newUser(firstname, lastname, state string, age int) (*User, error) {
+
+	if firstname == "" || lastname == "" || state == "" {
+		return nil, &UserError{Msg: "Invalid arguments"}
+	}
+
 	return &User{
 		Firstname: firstname,
 		Lastname:  lastname,
 		State:     state,
 		Age:       age,
-	}
+	}, nil
 }
 
 func main() {
-	person := newUser("John", "Doe", "Doe", 30)
+	person, err := newUser("John", "Doe", "Doe", 30)
+
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	fmt.Println(person.Lastname)
 }
