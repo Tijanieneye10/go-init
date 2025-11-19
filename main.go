da5fc099f73e8a50976f23f719612ad22bbdc654
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-11-19 13:03:12 +0100

    wip

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import "fmt"
+
+type Product struct {
+	ID          int
+	name        string
+	description string
+}
+
 func main() {
+	products := []Product{Product{
+		ID:          1,
+		name:        "Product 1",
+		description: "Description 1",
+	},
+		Product{
+			ID:          2,
+			name:        "Product 2",
+			description: "Description 2",
+		},
+	}
+
+	fmt.Println(products[0].name)
 }
