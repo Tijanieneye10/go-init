603b79fe730d3fddcc851677ebc565df8c704d08
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-11-19 23:05:08 +0100

    wip

---
+++ alias.go
@@ -0,0 +1,53 @@
+// package main
+
+// import "fmt"
+
+// type Product struct {
+// 	ID          int
+// 	name        string
+// 	description string
+// }
+
+// type StringOfString map[string]string
+
+// func (s StringOfString) log () StringOfString {
+// 	return s
+// }
+
+// func main() {
+
+// 	names := make(StringOfString, 2)
+
+// 	names["name"] = "John"
+// 	names["age"] = "Doe"
+// 	names["email"] = "john@example.com"
+// 	names["address"] = "123 Main St"
+
+// 	fmt.Println(names.log()["name"])
+// 	fmt.Println(len(names))
+
+// 	products := []Product{Product{
+// 		ID:          1,
+// 		name:        "Product 1",
+// 		description: "Description 1",
+// 	},
+// 		Product{
+// 			ID:          2,
+// 			name:        "Product 2",
+// 			description: "Description 2",
+// 		},
+// 	}
+
+// 	fmt.Println(products[0].name)
+
+// 	userDetails := map[string]string{
+// 		"name": "John Doe",
+// 		"age":  "30",
+// 		"city": "New York",
+// 	}
+
+// 	fmt.Println(userDetails["name"])
+// 	delete(userDetails, "age")
+// 	fmt.Println(userDetails["age"])
+
+// }
