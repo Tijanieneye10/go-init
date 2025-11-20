e98ff37fb87271dde4dff01843899b55a5d09ff0
Tijani Eneye Usman <brainyworld10@gmail.com>
2025-11-20 08:17:56 +0100

    wip

--- payments/main.go
+++ payments/main.go
@@ -1,52 +1,52 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"learning/payments"
-// )
+import (
+	"fmt"
+	"learning/payments"
+)
 
-// type Paymentable interface {
-// 	Initialize()
-// 	Pay()
-// }
+type Paymentable interface {
+	Initialize()
+	Pay()
+}
 
-// func main() {
-// 	paystack := payments.Paystack{Reference: 1, Amount: 100, Currency: "NGN"}
-// 	InitializePayment(&paystack)
-// 	Pay(&paystack)
+func main() {
+	paystack := payments.Paystack{Reference: 1, Amount: 100, Currency: "NGN"}
+	InitializePayment(&paystack)
+	Pay(&paystack)
 
-// 	flutterwave := payments.Flutterwave{Reference: 1, Amount: 100, Currency: "NGN"}
-// 	InitializePayment(&flutterwave)
-// 	Pay(&flutterwave)
+	flutterwave := payments.Flutterwave{Reference: 1, Amount: 100, Currency: "NGN"}
+	InitializePayment(&flutterwave)
+	Pay(&flutterwave)
 
-// 	unkown(3)
-// 	unkown("number")
-// }
+	unkown(3)
+	unkown("number")
+}
 
-// func InitializePayment(gateway Paymentable){
-// 	gateway.Initialize()
-// }
+func InitializePayment(gateway Paymentable){
+	gateway.Initialize()
+}
 
-// func Pay(gateway Paymentable){
-// 	gateway.Pay()
-// }
+func Pay(gateway Paymentable){
+	gateway.Pay()
+}
 
 
-// func unkown(param any) {
-// 	aint, aIsInt := param.(int)
-// 	if aIsInt {
-// 		fmt.Println("a is an int", aint)
-// 		return
-// 	}
-// 	bstring, bIsString := param.(string)
-// 	if bIsString {
-// 		fmt.Println("b is an string", bstring)
-// 		return
-// 	}
-// 	fmt.Println("Unknown type")
-// }
+func unkown(param any) {
+	aint, aIsInt := param.(int)
+	if aIsInt {
+		fmt.Println("a is an int", aint)
+		return
+	}
+	bstring, bIsString := param.(string)
+	if bIsString {
+		fmt.Println("b is an string", bstring)
+		return
+	}
+	fmt.Println("Unknown type")
+}
 
-// func generic[T any](param T) T {
-// 	fmt.Println(param)
-// 	return param
-// }
+func generic[T any](param T) T {
+	fmt.Println(param)
+	return param
+}
